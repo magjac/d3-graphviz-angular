7916d6c0760495291e2b714da357bf01190e675c
Magnus Jacobsson <Magnus.Jacobsson@berotec.se>
2019-04-19 14:29:45 +0200

    Add d3-graphviz demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+import { graphviz }  from 'd3-graphviz';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  title = 'd3-graphviz in Angular';
+
+  ngOnInit() {
+    graphviz('div').renderDot('digraph {a -> b}');
+  }
 }
